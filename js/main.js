e5c3e43699b01a93e12cb1723a045097bc17b5c9
Liliaaouissaoui <lilia.aouissaoui@studentfr.ch>
2023-11-26 19:15:15 +0100

    j'ai avancé comme je peux. -> pour lundi

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ var toggleMenu = function toggleMenu() {
 };
 
 burger.addEventListener("click", toggleMenu);
+
+let textEl;
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  textEl = document.querySelector(".animated-text");
+});
+
+//  ----
+
+function onMouseMove(e) {
+  const xRatio = e.x / window.innerWidth;
+  const yRatio = e.y / window.innerHeight;
+
+  const xPerc = 100 - xRatio * 100;
+  const yPerc = 100 - yRatio * 100;
+
+  textEl.style.backgroundPosition = `${xPerc}% ${yPerc}%`;
+}
+
+window.addEventListener("mousemove", onMouseMove);
