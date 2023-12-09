f504edf5e2d11ae617b189c6f334f927b5b54bc9
Liliaaouissaoui <lilia.aouissaoui@studentfr.ch>
2023-12-09 18:11:20 +0100

    plus que des détails en footer et c'est bon

--- js/main.js
+++ js/main.js
@@ -1,18 +1,51 @@
-var burger = document.querySelector(".burger");
-
-var menu = document.querySelector(".menu");
-
-var toggleMenu = function toggleMenu() {
-  menu.classList.toggle("is-active");
-  burger.classList.toggle("is-active");
-};
-
-burger.addEventListener("click", toggleMenu);
-
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 let textEl;
 
 document.addEventListener("DOMContentLoaded", (e) => {
   textEl = document.querySelector(".animated-text");
+});
+
+function mousemoveHandler(e) {
+  const width = this.offsetWidth;
+  const xPos = e.pageX;
+  const yPos = e.pageY;
+  const xPos2 = e.pageX - nextBtn.offsetLeft - nextBtnChild.offsetWidth;
+
+  if (xPos > width / 2) {
+    prevBtn.classList.remove(isVisible);
+    nextBtn.classList.add(isVisible);
+
+    nextBtnChild.style.left = `${xPos2}px`;
+    nextBtnChild.style.top = `${yPos}px`;
+  } else {
+    nextBtn.classList.remove(isVisible);
+    prevBtn.classList.add(isVisible);
+
+    prevBtnChild.style.left = `${xPos}px`;
+    prevBtnChild.style.top = `${yPos}px`;
+  }
+}
+
+function mouseleaveHandler() {
+  prevBtn.classList.remove(isVisible);
+  nextBtn.classList.remove(isVisible);
+}
+
+const listOfCardElements = document.querySelectorAll(".card");
+const cardContainer = document.querySelector(".card-container");
+
+listOfCardElements.forEach((cardElement, index) => {
+  cardElement.addEventListener("click", () => {
+    const scrollLeft = index * listOfCardElements[0].offsetWidth;
+    cardContainer.scrollTo({ left: scrollLeft, behavior: "smooth" });
+  });
 });
 
 //  ----
